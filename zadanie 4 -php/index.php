63e5fd6df03f412ea877dfdc65be9fd8af751d5e
Hlieb Vyshar <hvyshar@technikumlodz.pl>
2026-09-15 13:29:05 +0200

    zadanie 4 wykonane

---
+++ zadanie 4 -php/index.php
@@ -0,0 +1,44 @@
+<?php
+    // zadanie1
+    // $a=3;
+    // if($a%2==0){
+    //     echo"liczba parzysta";
+    // }else{
+    //     echo "liczba nieparzysta";
+    // }
+    // zadanie 2
+    // $a = 4;
+    // $b = 3;
+    // if($a%$b==0){
+    //     echo "liczba jest podzielna";
+    // }else{
+    //     echo "liczba nie jest";
+    // }
+    // zadanie 3
+    // $a=111;
+    // if($a>=1&&$a<=10){
+    //     echo "liczba wchodzi w przedzial od 1 do 10";
+    // }elseif($a>=17&&$a<=21){
+    //     echo "liczba wchodzi w przedzial od 17 do 21";
+    // }else{
+    //     echo "liczba nie wchodzi w żadny przedzial";
+    // }
+    // zadanie 4
+    // $a = -1;
+    // if($a>0){
+    //     echo "liczba wieksza od 0";
+    // }elseif($a==0){
+    //     echo "liczba rowna 0";
+    // }else{
+    //     echo "mniejsza od 0";
+    // }
+    // zadanie 5
+    // $wiek = 20;
+    // if($wiek<11){
+    //     echo "dziecko"; 
+    // }elseif($wiek>=11&&$wiek<=17){
+    //     echo "nastolatek";
+    // }else{
+    //     echo "dorosly";
+    // }
+?>
